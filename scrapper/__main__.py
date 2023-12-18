d51797fc68d38c4ea5e69953385984b2a9c4bbfe
Rafael Sabedra <152562421+rafaelssabedra@users.noreply.github.com>
2023-12-17 23:52:58 -0300

    Update __main__.py

--- scrapper/__main__.py
+++ scrapper/__main__.py
@@ -1,13 +1,3 @@
-"""
-Módulo Principal
-
-#ALTERAR
-Descrição: Este é o módulo com a função principal que integra as fases de entrada, processamento e saída de dados do Aplicativo de Plotagem.
-Autor: Letícia Santos e Ronaldo Debiasi
-Versão: 0.0.1
-Data: 04/12/2023
-
-"""
 
 import es
 import proc
